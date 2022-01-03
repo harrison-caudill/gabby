321c6fadc6ef74df76a731d73b010c7b102e7c16
Harrison Caudill <harrison@hypersphere.org>
2022-01-02 23:25:17 -0800

    add a simple numpy/pickle cache for computations

---
+++ gabby/cache.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import lmdb
+import numpy as np
+import os
+import pickle
+import pprint
+
+
+class GabbyCacheEntry(object):
+
+    def __init__(self, name, meta, mappings, is_dict):
+        self.name = name
+        self.meta = meta
+        self.mappings = mappings
+        self.is_dict = is_dict
+
+class GabbyCache(object):
+    """On-disk caching system.
+
+    pickle is hella slow compared to numpy serializing, so instead of
+    using pickle to serialize everything, we use a combination of
+    pickle and numpy.  Pickle works well for the metadata, but we'll
+    want to use numpy for the data.  Since you may have multiple numpy
+    arrays to save and dictionaries, we break out the caching
+    capabilities.
+
+    The data will be stored as follows:
+    <cache-dir>/<name>/user.pickle -- user-supplied metadata
+    <cache-dir>/<name>/meta.pickle -- cache-system metadata
+    <cache-dir>/<name>/0.np -- 1st numpy array
+    <cache-dir>/<name>/1.np -- 2nd numpy array
+    ...
+    <cache-dir>/<name>/(N-1).np -- Nth numpy array
+    """
+
+    def __init__(self, path):
+        """Does what every __init__ method does.
+
+        path: The <cache-dir> from the class docstring.
+        """
+        self.path = path
+
+    def _cache_path(self, name):
+        """Constructs the directory where the <name> would be cached.
+        """
+        return os.path.join(self.path, name)
+
+    def _meta_path(self, name):
+        return os.path.join(self._cache_path(name), 'metadata.pickle')
+
+    def _data_path(self, name):
+        return os.path.join(self._cache_path(name), 'data.np')
+
+    def put(self, name, metadata, data, overwrite=True):
+        """Adds the metadata and list of numpy arrays to the cache.
+
+        name: <str> the name to use when saving/referencing the data
+        metadata: <dict> whatever picklable metadata you want
+        data: Either an array or a dictionary of numpy arrays
+        overwrite: <bool>
+
+        The <data> entry can be either a dictionary or a list so
+        that you can name them if you want.  We'll just use numbers as
+        the implied names of the arrays when saving to disk, then
+        reconstruct the array as needed during a cache get.
+
+        Defaults to overwriting any old cache values, but you can
+        change that so it silently does nothing.
+        """
+
+        if self.is_cached(name):
+            if overwrite: self.clear_entry(name)
+            else: return
+
+        os.mkdir(self._cache_path(name))
+
+        is_dict = isinstance(data, dict)
+
+        if is_dict: mappings = sorted(data.keys())
+        else: mappings = list(range(len(data)))
+
+        ent = GabbyCacheEntry(name, metadata, mappings, is_dict)
+        with open(self._meta_path(name), 'wb') as fd: pickle.dump(ent, fd)
+
+        with open(self._data_path(name), 'wb') as fd:
+            for key in mappings: np.save(fd, data[key])
+
+    def get(self, name):
+        """Retreives the cached meta/data.
+
+        Whether you passed in an array or a dictionary for the data,
+        it'll be reloaded in the same order.
+
+        returns meta, data
+        """
+
+        if not self.is_cached(name): return None, None
+
+        ent = self._load_cache_meta(name)
+        with open(self._meta_path(name), 'rb') as fd: ent = pickle.load(fd)
+
+        if ent.is_dict: data = {}
+        else: data = [None for i in range(len(ent.mappings))]
+
+        with open(self._data_path(name), 'rb') as fd:
+            for key in ent.mappings: data[key] = np.load(fd)
+
+        return ent.meta, data
+
+    def _load_cache_meta(self, name):
+        with open(self._meta_path(name), 'rb') as fd: return pickle.load(fd)
+
+    def clear_entry(self, name):
+        """Deletes any cache entry for the name
+        """
+        if not self.is_cached(name): return
+
+        ent = self._load_cache_meta(name)
+        del_files = [self._meta_path(name), self._data_path(name)]
+        for path in del_files:
+            assert(os.path.isfile(path))
+            os.unlink(path)
+        os.rmdir(self._cache_path(name))
+
+    def is_cached(self, name):
+        """Determines whether or not the <name> is in the cache.
+        """
+        return os.path.isdir(self._cache_path(name))
---
+++ test/cache_test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import numpy as np
+import os
+import pprint
+import pytest
+import tempfile
+
+import gabby.cache
+
+
+@pytest.fixture
+def cache():
+    td = tempfile.TemporaryDirectory()
+    tmp = gabby.cache.GabbyCache(td.name)
+    setattr(tmp, '__tmpdir', td)
+    return tmp
+
+class TestCache(object):
+
+    def test_all_the_things(self, cache):
+        assert(os.path.isdir(cache.path))
+
+        # Hello array
+        data = [
+            np.zeros((2, 2,), dtype=np.float32),
+            np.zeros(1, dtype=np.int) + 1
+            ]
+        meta = {'foo':'bar'}
+        name = 'hello-array'
+        cache.put(name, meta, data)
+        rmeta, rdata = cache.get(name)
+        assert(rmeta == meta)
+        for i in range(len(data)):
+            assert((rdata[i]==data[i]).all())
+        assert(cache.is_cached(name))
+
+        # Make sure clearing works
+        cache.clear_entry(name)
+        assert(not cache.is_cached(name))
+
+        # Hello dict
+        data = {
+            'b': np.zeros((2, 2,), dtype=np.float32),
+            'a': np.zeros(1, dtype=np.int) + 1
+            }
+        meta = {'foo':'bar'}
+        name = 'hello-dict'
+        cache.put(name, meta, data)
+        rmeta, rdata = cache.get(name)
+        assert(rmeta == meta)
+        for k in data:
+            assert((rdata[k]==data[k]).all())
+
+        # Make sure overwrite works
+        data = {
+            'c': np.zeros((2, 2,), dtype=np.float32) + 2,
+            'd': np.zeros(1, dtype=np.int) + 3
+            }
+        meta = {'baz':'quux'}
+        cache.put(name, meta, data)
+        rmeta, rdata = cache.get(name)
+        assert(rmeta == meta)
+        for k in data:
+            assert((rdata[k]==data[k]).all())
+
+        # Make sure not-overwrite works
+        new_data = {
+            'e': np.zeros((2, 2,), dtype=np.float32) + 4,
+            'f': np.zeros(1, dtype=np.int) + 5
+            }
+        new_meta = {'what':'ever'}
+        cache.put(name, new_meta, new_data, overwrite=False)
+        rmeta, rdata = cache.get(name)
+        assert(rmeta == meta)
+        for k in data:
+            assert((rdata[k]==data[k]).all())
